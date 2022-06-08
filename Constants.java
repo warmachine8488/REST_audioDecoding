0f6e3e19beb0c7c72914f4c7c75af6705ba2822d
warmachine8488 <105331027+warmachine8488@users.noreply.github.com>
2022-06-08 19:07:56 +0530

    Create Constants.java
    This file contains All the constants

---
+++ Constants.java
@@ -0,0 +1,13 @@
+package com.bharath.spring.AsseblyAITest;
+
+public enum Constants {
+
+	API_KEY("https://api.assemblyai.com/v2/transcript");
+
+	String value;
+
+	Constants(String values) {
+		this.value = values;
+	}
+
+}
